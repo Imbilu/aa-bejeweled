378866f9f79432336b000392ae689c8a7ba20d42
sido <imbilusydney09@gmail.com>
2024-05-12 13:35:11 +0300

    wrote test-spec for bejeweled.js and their corresponding functions

--- class/bejeweled.js
+++ class/bejeweled.js
@@ -8,7 +8,7 @@ class Bejeweled {
     this.playerTurn = "O";
 
     // Initialize this
-    this.grid = [];
+    this.grid = this.generateRandomGrid(8, 8);
 
     this.cursor = new Cursor(8, 8);
 
@@ -19,9 +19,66 @@ class Bejeweled {
     Screen.render();
   }
 
+
+  generateRandomGrid(rows, cols) {
+    const grid = [];
+    const gemTypes = ['R', 'G', 'B', 'Y', 'M', 'C'];
+    for (let row = 0; row < rows; row++) {
+      grid[row] = [];
+      for (let col = 0; col < cols; col++) {
+        grid[row][col] = gemTypes[Math.floor(Math.random() * gemTypes.length)];
+      }
+    }
+    return grid;
+  }
+
+  swap(row1, col1, row2, col2) {
+    let tmp = this.grid[row1][col1];
+    this.grid[row1][col1] = this.grid[row2][col2];
+    this.grid[row2][col2] = tmp;
+  }
+
+
+  isValidSwap(row1, col1, row2, col2) {
+    if (row1 === row2 && Math.abs(col1 - col2) !== 1) {
+      return false;
+    }
+    else if (col1 === col2 && Math.abs(row1 - row2) !== 1) {
+      return false;
+    }
+    else if (col1 !== col2 && row1 !== row2) {
+      return false;
+    }
+    return true;
+  }
+
   static checkForMatches(grid) {
 
     // Fill this in
+    let hasMatch = false;
+
+    // Check for horizontal matches
+    for (let row = 0; row < grid.length; row++) {
+      for (let col = 0; col < grid[row].length - 2; col++) {
+        const gem = grid[row][col];
+        if (gem === grid[row][col + 1] && gem === grid[row][col + 2]) {
+          hasMatch = true;
+          // Mark matched gems for removal (logic can be implemented later)
+        }
+      }
+    }
+
+    // Check for vertical matches
+    for (let row = 0; row < grid.length-2; row++) {
+      for (let col = 0; col < grid.length; col++) {
+        const gem = grid[row][col];
+        if (gem === grid[row +1][col] && gem === grid[row+2][col]) {
+          hasMatch = true;
+        }
+      }
+    }
+
+    return hasMatch;
 
   }
 
--- test/bejeweled-spec.js
+++ test/bejeweled-spec.js
@@ -1,16 +1,102 @@
 const { expect } = require('chai');
+const { Cursor } = require('../class/cursor.js');
 
 const Bejeweled = require("../class/bejeweled.js");
 
 describe ('Bejeweled', function () {
 
+  let board;
+
+  beforeEach(function() {
+    board = new Bejeweled();
+  });
   // Add tests for setting up a basic board
+  describe('constructor function', () => {
+    it('should create a new 8 x 8 board', () => {
+      expect(board.grid).to.have.length(8);
+      expect(board.grid).to.have.length(8);
+    });
+  });
 
   // Add tests for a valid swap that matches 3
+  describe('swap function', () => {
+    it('should swap adjacent elements horizontally', () => {
+      board.grid = [
+        ['R', 'G', 'B', 'Y'],
+        ['G', 'G', 'Y', 'G'],
+        ['B', 'R', 'B', 'Y'],
+        ['Y', 'G', 'Y', 'R'],
+      ];
+      board.swap(0, 0, 0, 1); // Swap 'R' with 'G'
+      const expectedGrid = [
+        ['G', 'R', 'B', 'Y'],
+        ['G', 'G', 'Y', 'G'],
+        ['B', 'R', 'B', 'Y'],
+        ['Y', 'G', 'Y', 'R'],
+      ];
+      expect(board.grid).to.deep.equal(expectedGrid);
+    });
 
-  // Add tests for swaps that set up combos
+    it('should swap elements vertically', () => {
+      board.grid = [
+        ['R', 'G', 'B', 'Y'],
+        ['G', 'G', 'G', 'Y'],
+        ['B', 'R', 'B', 'Y'],
+        ['Y', 'G', 'Y', 'R'],
+      ];
+      board.swap(1, 1, 2, 1); // Swap middle 'G' with 'R' below
+      const expectedGrid = [
+        ['R', 'G', 'B', 'Y'],
+        ['G', 'R', 'G', 'Y'],
+        ['B', 'G', 'B', 'Y'],
+        ['Y', 'G', 'Y', 'R'],
+      ];
+
+
+      expect(board.grid).to.deep.equal(expectedGrid);
+    });
+  });
+
 
   // Add tests to check if there are no possible valid moves
+  describe('isValid function', () => {
+    it('should return false if a swap is invalid', () => {
+      board.grid = [
+        ['R', 'G', 'B', 'Y'],
+        ['G', 'R', 'B', 'Y'],
+        ['B', 'G', 'R', 'Y'],
+        ['Y', 'B', 'G', 'R'],
+      ];
+      expect(board.isValidSwap(0, 0, 0, 2)).to.equal(false);
+      expect(board.isValidSwap(0, 0, 1, 2)).to.equal(false);
+      expect(board.isValidSwap(1, 0, 0, 2)).to.equal(false);
+    });
+  });
+
+  describe('checkForMatches function', () => {
+    it('should return true if there is a horizontal three match', () => {
+      board.grid = [
+        ['G', 'R', 'B', 'Y'],
+        ['G', 'G', 'G', 'R'],
+        ['B', 'R', 'B', 'Y'],
+        ['Y', 'G', 'Y', 'R'],
+      ];
+
+      let match = Bejeweled.checkForMatches(board.grid);
+      expect(match).to.eq(true);
+    });
+
+    it('should return true if there is a vertical three match', () => {
+      board.grid = [
+        ['G', 'R', 'B', 'Y'],
+        ['G', 'G', 'R', 'Y'],
+        ['B', 'R', 'B', 'Y'],
+        ['Y', 'G', 'Y', 'R'],
+      ];
+
+      let match = Bejeweled.checkForMatches(board.grid);
+      expect(match).to.eq(true);
+    });
+  });
 
 });
-
